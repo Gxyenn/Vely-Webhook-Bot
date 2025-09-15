0db8ee016d4eceaff1f738e60562024284ec2560
Gxyenn 正式 <GxyennDEV@gmail.com>
2025-09-15 14:39:04 +0700

    Add Animation component

---
+++ client/src/components/Animation.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Animation = () => (
+  <div className="w-32 h-32 mx-auto my-8">
+    <div className="w-full h-full bg-blue-500 rounded-full animate-bounce"></div>
+  </div>
+);
+
+export default Animation;
