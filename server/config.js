02e4c99858ba8b6b4b29ba8349e8fa68ebd67551
Gxyenn 正式 <GxyennDEV@gmail.com>
2025-09-15 14:37:18 +0700

    Add server/config.js with configuration settings

---
+++ server/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  SMTP_USER: "your_gmail@gmail.com",
+  SMTP_PASS: "your_smtp_password",
+  MONGODB_URI: "mongodb+srv://user:pass@cluster.mongodb.net/velytools",
+  GEMINI_APIKEY: "your_google_gemini_api_key",
+  VERCEL_TOKEN: "your_vercel_api_token",
+  CREATOR: "Gxyenn"
+}
